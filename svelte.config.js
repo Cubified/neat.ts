4cb5a943950eb67a5afd698953a47c6e6ffa3638
Andrew <Cubified@users.noreply.github.com>
2023-09-15 21:24:47 -0400

    Add Github action, remove docs/

--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,8 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-      pages: 'docs',
-			assets: 'docs',
+      pages: 'build',
+			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true
